63261d0043a4f09f065637f112851897cebfdddc
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-02-07 01:08:42 +0100

    First plugin: let’s add some validation

---
+++ Dynamics365/Training.Plugins/ValidationPlugin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Training.Plugins
+{
+    public class ValidationPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var entity = (Entity)context.InputParameters["Target"];
+            var name = (string)entity["name"];
+            if ("Test".Equals(name))
+            {
+                throw new InvalidPluginExecutionException("Cannot use this name");
+            }
+        }
+    }
+}
