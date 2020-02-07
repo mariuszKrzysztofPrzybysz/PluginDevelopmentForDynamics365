32918b4fc061970e386430e671075c28f09be9e3
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-02-07 12:27:39 +0100

    OrganizationService

---
+++ Dynamics365/TrainingConfiguration.Plugin/ita_notestallowedmessage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace TrainingConfiguration.Plugin
+{
+    public class ita_notestallowedmessage : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var entity = (Entity)context.InputParameters["Target"];
+            if (entity.Contains("ita_notestallowedmessage"))
+            {
+                if (entity["ita_notestallowedmessage"] is null)
+                {
+                    throw new InvalidPluginExecutionException("Cannot be null");
+                }
+            }
+        }
+    }
+}
