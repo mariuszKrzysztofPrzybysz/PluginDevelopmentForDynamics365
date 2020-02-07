87cae13b05d7cd37ada0142565f38ad91c0ae2cc
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-02-07 09:49:47 +0100

    First plugin updated: let’s make it smarter

--- Dynamics365/Training.Plugins/ValidationPlugin.cs
+++ Dynamics365/Training.Plugins/ValidationPlugin.cs
@@ -11,10 +11,13 @@ namespace Training.Plugins
                 serviceProvider.GetService(typeof(IPluginExecutionContext));
 
             var entity = (Entity)context.InputParameters["Target"];
-            var name = (string)entity["ita_name"];
-            if ("Test".Equals(name))
+            if (entity.Contains("ita_name"))
             {
-                throw new InvalidPluginExecutionException("Cannot use this name");
+                var name = (string)entity["ita_name"];
+                if ("Test".Equals(name))
+                {
+                    throw new InvalidPluginExecutionException("Cannot use this name");
+                }
             }
         }
     }
