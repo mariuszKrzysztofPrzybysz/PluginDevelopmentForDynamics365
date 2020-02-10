cb0ebd9e6f0375bc1b78bd3b1de5310348839641
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-02-10 10:18:46 +0100

    Plugin #1: Pushing priority field updates to the related company contacts

---
+++ Dynamics365/TrainingCompany.Plugin/TrainingCompanyPlugin.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace TrainingCompany.Plugin
+{
+    public class TrainingCompanyPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var entity = (Entity)context.InputParameters["Target"];
+            if(entity.Contains("ita_priority"))
+            {
+                throw new InvalidPluginExecutionException("test");
+            }
+        }
+    }
+}
