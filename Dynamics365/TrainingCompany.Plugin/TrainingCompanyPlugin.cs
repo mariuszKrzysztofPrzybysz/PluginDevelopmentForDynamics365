bdb9e216160e6384e6a8848372fc7712b4cbf9c0
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-02-10 15:15:17 +0100

    Setting it up

--- Dynamics365/TrainingCompany.Plugin/TrainingCompanyPlugin.cs
+++ Dynamics365/TrainingCompany.Plugin/TrainingCompanyPlugin.cs
@@ -8,50 +8,33 @@ namespace TrainingCompany.Plugin
     {
         public void Execute(IServiceProvider serviceProvider)
         {
-            var tracingService = (ITracingService)
-                serviceProvider.GetService(typeof(ITracingService));
-
             var context = (IPluginExecutionContext)
-            serviceProvider.GetService(typeof(IPluginExecutionContext));
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationService service = null;
+            IOrganizationServiceFactory serviceFactory = null;
 
             var entity = (Entity)context.InputParameters["Target"];
-            if (entity.Contains("ita_priority"))
+            bool creditRatingCheck = entity.Contains("ita_creditrating") && entity["ita_creditrating"] != null;
+            bool priorityUpdate = context.MessageName.Equals("Update") && entity.Contains("ita_priority");
+
+            if (creditRatingCheck || priorityUpdate)
             {
-                var serviceFactory = (IOrganizationServiceFactory)
+                serviceFactory = (IOrganizationServiceFactory)
                     serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                service = serviceFactory.CreateOrganizationService(context.UserId);
+            }
 
-                var service = serviceFactory.CreateOrganizationService(context.UserId);
-
-                int pageNumber = 1;
-                string pagingCookie = string.Empty;
-                EntityCollection result;
-                do
-                {
-                    result = RetrieveContacts(tracingService, service, entity.Id, pageNumber, pagingCookie);
-                    foreach (var e in result.Entities)
-                    {
-                        tracingService.Trace(e.Id.ToString());
-                        var updatedContact = new Entity(e.LogicalName)
-                        {
-                            Id = e.Id
-                        };
-                        updatedContact["ita_priority"] = entity["ita_priority"];
-                        service.Update(updatedContact);
-
-                        /*
-                        So why creating a new in-memory etity object?
-                        The reason is that, if you call update on the entity retrieved from the query, there might be some other
-                        attributes, and, even though you won’t be changing them, they will still be sent back to the server
-                        */
-                    }
-                    pagingCookie = result.PagingCookie;
-                    pageNumber++;
-                }
-                while (result.MoreRecords);
+            if (creditRatingCheck)
+            {
+                ExecuteCreditLimitValidation(context, service, entity);
+            }
+            if (priorityUpdate)
+            {
+                ExecutePriorityUpdate(context, service, entity);
             }
         }
 
-        private EntityCollection RetrieveContacts(ITracingService tracingService, IOrganizationService organizationService, Guid companyId, int page, string pagingCookie)
+        private EntityCollection RetrieveContacts(IOrganizationService organizationService, Guid companyId, int page, string pagingCookie)
         {
             var qe = new QueryExpression("ita_trainingcontact");
             qe.Criteria.AddCondition(new ConditionExpression("ita_company", ConditionOperator.Equal, companyId));
@@ -66,6 +49,40 @@ namespace TrainingCompany.Plugin
 
             var result = organizationService.RetrieveMultiple(qe);
             return result;
+        }
+
+        private void ExecuteCreditLimitValidation(IPluginExecutionContext context, IOrganizationService service, Entity entity)
+        {
+
+        }
+
+        private void ExecutePriorityUpdate(IPluginExecutionContext context, IOrganizationService service, Entity entity)
+        {
+            int pageNumber = 1;
+            string pagingCookie = string.Empty;
+            EntityCollection result;
+            do
+            {
+                result = RetrieveContacts(service, entity.Id, pageNumber, pagingCookie);
+                foreach (var e in result.Entities)
+                {
+                    var updatedContact = new Entity(e.LogicalName)
+                    {
+                        Id = e.Id
+                    };
+                    updatedContact["ita_priority"] = entity["ita_priority"];
+                    service.Update(updatedContact);
+
+                    /*
+                    So why creating a new in-memory etity object?
+                    The reason is that, if you call update on the entity retrieved from the query, there might be some other
+                    attributes, and, even though you won’t be changing them, they will still be sent back to the server
+                    */
+                }
+                pagingCookie = result.PagingCookie;
+                pageNumber++;
+            }
+            while (result.MoreRecords);
         }
     }
 }
