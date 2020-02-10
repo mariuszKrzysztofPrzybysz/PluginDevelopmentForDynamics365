869eae93ffc5115e38a0c6c9cbd67b8896eab351
Mariusz Krzysztof Przybysz <mariusz.krzysztof.przybysz@gmail.com>
2020-02-10 12:13:57 +0100

    Record Count, Paging, and Total Record Count

--- Dynamics365/TrainingCompany.Plugin/TrainingCompanyPlugin.cs
+++ Dynamics365/TrainingCompany.Plugin/TrainingCompanyPlugin.cs
@@ -8,8 +8,11 @@ namespace TrainingCompany.Plugin
     {
         public void Execute(IServiceProvider serviceProvider)
         {
+            var tracingService = (ITracingService)
+                serviceProvider.GetService(typeof(ITracingService));
+
             var context = (IPluginExecutionContext)
-                serviceProvider.GetService(typeof(IPluginExecutionContext));
+            serviceProvider.GetService(typeof(IPluginExecutionContext));
 
             var entity = (Entity)context.InputParameters["Target"];
             if (entity.Contains("ita_priority"))
@@ -19,37 +22,63 @@ namespace TrainingCompany.Plugin
 
                 var service = serviceFactory.CreateOrganizationService(context.UserId);
 
-                var fetchXml =
-                    @"<fetch version=""1.0"" output-format=""xml-platform"" mapping=""logical"" distinct=""false"">
-                        < entity name = ""ita_trainingcompany"" >
-                            < attribute name = ""ita_trainingcompanyid"" />
-                            < attribute name = ""ita_name"" />
-                            < attribute name = ""createdon"" />
-                            < order attribute = ""ita_name"" descending = ""false"" />
-                            < filter type = ""and"" >
-                                < condition attribute = ""ita_company"" operator= ""eq"" value = ""{0}"" />
-                            </ filter >
-                        </ entity >
-                    </ fetch > ";
-                fetchXml = string.Format(fetchXml, entity.Id);
-                var qe = new FetchExpression(fetchXml);
-                var result = service.RetrieveMultiple(qe);
-                foreach (var e in result.Entities)
+                int pageNumber = 1;
+                string pagingCookie = string.Empty;
+                EntityCollection result;
+                do
                 {
-                    var updatedContact = new Entity(e.LogicalName);
-                    updatedContact.Id = e.Id;
-                    updatedContact["ita_priority"] = entity["ita_priority"];
-                    service.Update(updatedContact);
+                    result = RetrieveContacts(tracingService, service, entity.Id, pageNumber, pagingCookie);
+                    foreach (var e in result.Entities)
+                    {
+                        tracingService.Trace(e.Id.ToString());
+                        var updatedContact = new Entity(e.LogicalName)
+                        {
+                            Id = e.Id
+                        };
+                        updatedContact["ita_priority"] = entity["ita_priority"];
+                        service.Update(updatedContact);
 
-                    /*
-                    So why creating a new in-memory etity object?
-                    The reason is that, if you call update on the entity retrieved from the query, there might be some other
-                    attributes, and, even though you won’t be changing them, they will still be sent back to the server
-                    */
-
-                    service.Update(e);
+                        /*
+                        So why creating a new in-memory etity object?
+                        The reason is that, if you call update on the entity retrieved from the query, there might be some other
+                        attributes, and, even though you won’t be changing them, they will still be sent back to the server
+                        */
+                    }
+                    pagingCookie = result.PagingCookie;
+                    pageNumber++;
                 }
+                while (result.MoreRecords);
             }
+        }
+
+        private EntityCollection RetrieveContacts(ITracingService tracingService, IOrganizationService organizationService, Guid companyId, int page, string pagingCookie)
+        {
+            if (!string.IsNullOrWhiteSpace(pagingCookie))
+            {
+                pagingCookie = pagingCookie.Replace("\"", "'").Replace(">", "&gt;").Replace("<", "&lt;");
+            }
+            var fetchXml =
+                @"<fetch version=""1.0""
+                    count=""5""
+                    page=""{1}""
+                    paging-cookie=""{2}""
+                    returntotalrecordcount=""true""
+                    output-format=""xml-platform""
+                    mapping=""logical""
+                    distinct=""false"">
+                    <entity name=""ita_trainingcontact"">
+                        <attribute name=""ita_trainingcontactid"" />
+                        <filter type=""and"">
+                        <condition attribute=""ita_company"" operator=""eq"" value=""{0}"" />
+                        </filter>
+                    </entity>
+                </fetch>";
+
+            fetchXml = string.Format(fetchXml, companyId, page, pagingCookie);
+            tracingService.Trace(fetchXml);
+            var qe = new FetchExpression(fetchXml);
+            var result = organizationService.RetrieveMultiple(qe);
+            return result;
         }
     }
 }
